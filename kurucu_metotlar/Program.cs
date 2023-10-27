e5bfe0cdfe45614b4857afb10de370700acb40b8
romrem7 <127850740+romrem7@users.noreply.github.com>
2023-10-27 23:30:35 +0300

    Add files via upload

---
+++ kurucu_metotlar/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+       //Söz Dizimi
+       // class SinifAdi
+    //    {
+    //     [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+    //     [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+    //     {
+    //         //Metot Komutları
+    //     }
+    //    }
+
+    //Erişim Belirleyiciler
+    // *Public
+    // *Private
+    // *Internal
+    // *Protected
+
+    Console.WriteLine("*******Çalışan 1*******");
+    Calisan calisan1 = new Calisan("Ayşe", "Kara", 23415634, "İnsan Kaynkları" );
+    calisan1.CalisanBilgileri();
+    
+    Console.WriteLine("*******Çalışan 2*******");
+    Calisan calisan2 = new Calisan();
+    calisan2.Ad ="Deniz";
+    calisan2.Soyad="Arda";
+    calisan2.No=256123698;
+    calisan2.Departman="Satın Alma";
+    calisan2.CalisanBilgileri();
+
+    Console.WriteLine("*******Çalışan 3*******");
+    Calisan calisan3 = new Calisan("İrem", "Dübüş");
+    calisan3.CalisanBilgileri();
+    }
+}
+
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public Calisan(string ad, string soyad, int no, string departman)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+        this.No = no;
+        this.Departman = departman;
+       
+    }
+
+    public Calisan(string ad, string soyad)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+       
+    }
+
+    public Calisan(){}
+    
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Çalışanın Adı:{0}", Ad);
+        Console.WriteLine("Çalışanın Soyadı:{0}", Soyad);
+        Console.WriteLine("Çalışanın Numarası:{0}", No);
+        Console.WriteLine("Çalışanın Departmanı:{0}", Departman);
+    }
+}
